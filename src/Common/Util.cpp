5bd09f4030ad4899145d15486caa959843ddb6d5
nnian <35841252+orangecoi@users.noreply.github.com>
2018-04-01 13:29:56 +0200

    Fix in foldername

--- src/Common/Util.cpp
+++ src/Common/Util.cpp
@@ -306,7 +306,7 @@ std::string get_nix_version_display_string()
     std::string config_folder;
 #ifdef WIN32
     // Windows
-    config_folder = get_special_folder_path(CSIDL_APPDATA, true) + "/" + CryptoNote::CRYPTONOTE_NAME + "-gui";
+    config_folder = get_special_folder_path(CSIDL_APPDATA, true) + "/" + CryptoNote::CRYPTONOTE_NAME + "-v2";
 #else
     std::string pathRet;
     char* pszHome = getenv("HOME");
@@ -317,10 +317,10 @@ std::string get_nix_version_display_string()
 #ifdef MAC_OSX
     // Mac
     pathRet /= "Library/Application Support";
-    config_folder =  (pathRet + "/" + CryptoNote::CRYPTONOTE_NAME + "-gui");
+    config_folder =  (pathRet + "/" + CryptoNote::CRYPTONOTE_NAME + "-v2");
 #else
     // Unix
-    config_folder = (pathRet + "/." + CryptoNote::CRYPTONOTE_NAME + "-gui");
+    config_folder = (pathRet + "/." + CryptoNote::CRYPTONOTE_NAME + "-v2");
 #endif
 #endif
 
