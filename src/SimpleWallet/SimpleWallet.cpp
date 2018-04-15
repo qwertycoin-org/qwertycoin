3d3b1b8c1a80f882192cff8e0d19a5ca7c38888b
nnian <35841252+orangecoi@users.noreply.github.com>
2018-04-15 15:37:22 +0200

    change in payment url tag

--- src/SimpleWallet/SimpleWallet.cpp
+++ src/SimpleWallet/SimpleWallet.cpp
@@ -527,8 +527,8 @@ bool processServerAliasResponse(const std::string& s, std::string& address) {
 	try {
 
 		// Courtesy of Monero Project
-		// make sure the txt record has "oa1:krb" and find it
-		auto pos = s.find("oa1:krb");
+		// make sure the txt record has "oa1:qwc" and find it
+		auto pos = s.find("oa1:qwc");
 		if (pos == std::string::npos)
 			return false;
 		// search from there to find "recipient_address="
