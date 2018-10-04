f5f297bb6cd73933b8d8017779395c3fc81f9d8e
Jake Smith <6624930+nnian@users.noreply.github.com>
2018-10-04 19:53:14 +0200

    Read output keys for WalletLegacy

--- src/WalletLegacy/WalletLegacy.cpp
+++ src/WalletLegacy/WalletLegacy.cpp
@@ -110,6 +110,8 @@ private:
 
 } //namespace
 
+using namespace Logging;
+
 namespace CryptoNote {
 
 class SyncStarter : public CryptoNote::IWalletLegacyObserver {
@@ -290,6 +292,19 @@ void WalletLegacy::doLoad(std::istream& source) {
     } catch (const std::exception&) {
       // ignore cache loading errors
     }
+
+
+  // Read all output keys cache
+    std::vector<TransactionOutputInformation> allTransfers;
+    m_transferDetails->getOutputs(allTransfers, ITransfersContainer::IncludeAll);
+    auto message = "Loaded " + std::to_string(allTransfers.size()) + " known transfer(s)\r\n";
+    m_loggerGroup("WalletLegacy", INFO, boost::posix_time::second_clock::local_time(), message);
+    for (auto& o : allTransfers) {
+      if (o.type == TransactionTypes::OutputType::Key) {
+        m_transfersSync.addPublicKeysSeen(m_account.getAccountKeys().address, o.transactionHash, o.outputKey);
+      }
+    }
+    
   } catch (std::system_error& e) {
     runAtomic(m_cacheMutex, [this] () {this->m_state = WalletLegacy::NOT_INITIALIZED;} );
     m_observerManager.notify(&IWalletLegacyObserver::initCompleted, e.code());
