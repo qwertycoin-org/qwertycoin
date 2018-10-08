2ba359f55afd1626dac12b2449b6dde84e4bfed9
Jake Smith <6624930+nnian@users.noreply.github.com>
2018-10-08 12:51:56 +0200

    DNS Checkpoints add missing vector

--- src/Common/DnsTools.h
+++ src/Common/DnsTools.h
@@ -18,17 +18,14 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace Common {
-
-//class DnsTools
-//{
-//public:
 
 #ifndef __ANDROID__
 
   bool fetch_dns_txt(const std::string domain, std::vector<std::string>&records);
 
 #endif
-//}
+
 }
