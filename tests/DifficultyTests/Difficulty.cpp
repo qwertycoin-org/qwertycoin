25521de2370df44686cc84ca1e8f872797b0fbc4
Alexander <speqtr@users.noreply.github.com>
2019-03-06 16:40:59 +0300

    Fixed compilation issues in tests/DifficultyTests

--- tests/DifficultyTests/Difficulty.cpp
+++ tests/DifficultyTests/Difficulty.cpp
@@ -57,7 +57,7 @@ int main(int argc, char *argv[]) {
             end = n - currency.difficultyLag();
             begin = end - currency.difficultyWindow();
         }
-        uint64_t res = currency.nextDifficulty(
+        uint64_t res = currency.nextDifficultyV5(
 			CryptoNote::BLOCK_MAJOR_VERSION_1,
             vector<uint64_t>(timestamps.begin() + begin, timestamps.begin() + end),
             vector<uint64_t>(cumulative_difficulties.begin() + begin, cumulative_difficulties.begin() + end));
