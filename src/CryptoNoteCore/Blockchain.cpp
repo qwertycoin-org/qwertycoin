0f44ef7bbfff5c4ce845c0f4915a87dc07225338
Mr QWC <alexnnian@protonmail.com>
2019-03-20 10:44:03 +0100

    Added height to "Saving blockchain..." message

--- src/CryptoNoteCore/Blockchain.cpp
+++ src/CryptoNoteCore/Blockchain.cpp
@@ -591,7 +591,7 @@ void Blockchain::rebuildCache() {
 bool Blockchain::storeCache() {
   std::lock_guard<decltype(m_blockchain_lock)> lk(m_blockchain_lock);
 
-  logger(INFO, BRIGHT_WHITE) << "Saving blockchain...";
+  logger(INFO, BRIGHT_WHITE) << "Saving blockchain at height " << m_blocks.size() - 1 << "...";
   BlockCacheSerializer ser(*this, getTailId(), logger.getLogger());
   if (!ser.save(appendPath(m_config_folder, m_currency.blocksCacheFileName()))) {
     logger(ERROR, BRIGHT_RED) << "Failed to save blockchain cache";
