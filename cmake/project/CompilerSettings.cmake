c01acbbfc16564077cfda7ca26f8d1dceb1ee58b
Alexander <speqtr@users.noreply.github.com>
2019-04-13 21:09:49 +0300

    Updated compiler settings

--- cmake/project/CompilerSettings.cmake
+++ cmake/project/CompilerSettings.cmake
@@ -1,7 +1,15 @@
+if(NOT CMAKE_C_STANDARD)
+    set(CMAKE_C_STANDARD 11)
+    set(CMAKE_C_STANDARD_REQUIRED ON)
+endif()
+
 if(NOT CMAKE_CXX_STANDARD)
     set(CMAKE_CXX_STANDARD 14)
     set(CMAKE_CXX_STANDARD_REQUIRED ON)
 endif()
+
+# Don't use e.g. GNU extension (like -std=gnu++11) for portability
+set(CMAKE_CXX_EXTENSIONS OFF)
 
 if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
     # Clang and AppleClang
