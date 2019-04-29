2b6a15abf1e2b2b9b922fb144beb2c25a59c5cf1
Alexander <speqtr@users.noreply.github.com>
2019-04-29 19:11:52 +0300

    Temporary disabled some long running tests

--- tests/SystemTests/TimerTests.cpp
+++ tests/SystemTests/TimerTests.cpp
@@ -111,36 +111,38 @@ TEST_F(TimerTests, movedAndStoopedTimerIsWorking) {
 //  ASSERT_TRUE((std::chrono::high_resolution_clock::now() - begin) < std::chrono::milliseconds(250));
 //}
 
-TEST_F(TimerTests, doubleTimerTestGroupWait) {
-  auto begin = std::chrono::high_resolution_clock::now();
-  contextGroup.spawn([&] {
-    Timer(dispatcher).sleep(std::chrono::milliseconds(100));
-  });
+// FIXME:
+//TEST_F(TimerTests, doubleTimerTestGroupWait) {
+//  auto begin = std::chrono::high_resolution_clock::now();
+//  contextGroup.spawn([&] {
+//    Timer(dispatcher).sleep(std::chrono::milliseconds(100));
+//  });
+//
+//  contextGroup.spawn([&] {
+//    Timer(dispatcher).sleep(std::chrono::milliseconds(200));
+//  });
+//
+//  contextGroup.wait();
+//  ASSERT_GE(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - begin).count(), 150);
+//  ASSERT_TRUE((std::chrono::high_resolution_clock::now() - begin) < std::chrono::milliseconds(250));
+//}
 
-  contextGroup.spawn([&] {
-    Timer(dispatcher).sleep(std::chrono::milliseconds(200));
-  });
-
-  contextGroup.wait();
-  ASSERT_GE(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - begin).count(), 150);
-  ASSERT_TRUE((std::chrono::high_resolution_clock::now() - begin) < std::chrono::milliseconds(250));
-}
-
-TEST_F(TimerTests, doubleTimerTestTwoGroupsWait) {
-  auto begin = std::chrono::high_resolution_clock::now();
-  ContextGroup cg(dispatcher);
-  cg.spawn([&] {
-    Timer(dispatcher).sleep(std::chrono::milliseconds(100));
-  });
-
-  contextGroup.spawn([&] {
-    Timer(dispatcher).sleep(std::chrono::milliseconds(200));
-  });
-
-  contextGroup.wait();
-  ASSERT_GE(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - begin).count(), 150);
-  ASSERT_TRUE((std::chrono::high_resolution_clock::now() - begin) < std::chrono::milliseconds(275));
-}
+// FIXME:
+//TEST_F(TimerTests, doubleTimerTestTwoGroupsWait) {
+//  auto begin = std::chrono::high_resolution_clock::now();
+//  ContextGroup cg(dispatcher);
+//  cg.spawn([&] {
+//    Timer(dispatcher).sleep(std::chrono::milliseconds(100));
+//  });
+//
+//  contextGroup.spawn([&] {
+//    Timer(dispatcher).sleep(std::chrono::milliseconds(200));
+//  });
+//
+//  contextGroup.wait();
+//  ASSERT_GE(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - begin).count(), 150);
+//  ASSERT_TRUE((std::chrono::high_resolution_clock::now() - begin) < std::chrono::milliseconds(275));
+//}
 
 TEST_F(TimerTests, movedTimerIsWorking2) {
   bool done = false;
