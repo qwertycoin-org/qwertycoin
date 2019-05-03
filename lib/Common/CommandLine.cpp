aa9a685bf7f45ad5e8a50647fa2528cf596a33b4
Alexander <speqtr@users.noreply.github.com>
2019-05-03 20:27:26 +0300

    Fixed code style issues in Common/CommandLine

--- lib/Common/CommandLine.cpp
+++ lib/Common/CommandLine.cpp
@@ -18,9 +18,10 @@
 
 #include "CommandLine.h"
 
-namespace command_line
-{
-  const arg_descriptor<bool> arg_help = {"help", "Produce help message"};
-  const arg_descriptor<bool> arg_version = {"version", "Output version information"};
-  const arg_descriptor<std::string> arg_data_dir = {"data-dir", "Specify data directory"};
-}
+namespace command_line {
+
+const arg_descriptor<bool> arg_help = { "help", "Produce help message" };
+const arg_descriptor<bool> arg_version = { "version", "Output version information" };
+const arg_descriptor<std::string> arg_data_dir = { "data-dir", "Specify data directory" };
+
+} // namespace command_line
