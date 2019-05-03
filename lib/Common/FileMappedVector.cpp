a7ff98012d13158d76d9a98e1c55888dc6cf37bc
Alexander <speqtr@users.noreply.github.com>
2019-05-03 21:45:46 +0300

    Fixed code style issues in Common/FileMappedVector

--- lib/Common/FileMappedVector.cpp
+++ lib/Common/FileMappedVector.cpp
@@ -16,8 +16,10 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with Qwertycoin.  If not, see <http://www.gnu.org/licenses/>.
 
-#include "FileMappedVector.h"
+#include <Common/FileMappedVector.h>
 
 namespace {
+
 char suppressMSVCWarningLNK4221;
-}
+
+} // namespace
