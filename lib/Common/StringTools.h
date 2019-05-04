6c00518f8a5a3faa49d1ded5c922a27a5c4566d9
Alexander <speqtr@users.noreply.github.com>
2019-05-04 21:18:40 +0300

    Fixed code style issues in Common/StringTools

--- lib/Common/StringTools.h
+++ lib/Common/StringTools.h
@@ -26,97 +26,136 @@
 
 namespace Common {
 
-std::string asString(const void* data, size_t size); // Does not throw
-std::string asString(const std::vector<uint8_t>& data); // Does not throw
-std::vector<uint8_t> asBinaryArray(const std::string& data);
+std::string asString(const void *data, size_t size);
+std::string asString(const std::vector<uint8_t> &data);
 
-uint8_t fromHex(char character); // Returns value of hex 'character', throws on error
-bool fromHex(char character, uint8_t& value); // Assigns value of hex 'character' to 'value', returns false on error, does not throw
-size_t fromHex(const std::string& text, void* data, size_t bufferSize); // Assigns values of hex 'text' to buffer 'data' up to 'bufferSize', returns actual data size, throws on error
-bool fromHex(const std::string& text, void* data, size_t bufferSize, size_t& size); // Assigns values of hex 'text' to buffer 'data' up to 'bufferSize', assigns actual data size to 'size', returns false on error, does not throw
-std::vector<uint8_t> fromHex(const std::string& text); // Returns values of hex 'text', throws on error
-bool fromHex(const std::string& text, std::vector<uint8_t>& data); // Appends values of hex 'text' to 'data', returns false on error, does not throw
+std::vector<uint8_t> asBinaryArray(const std::string &data);
+
+// Returns value of hex 'character', throws on error
+uint8_t fromHex(char character);
+
+// Assigns value of hex 'character' to 'value', returns false on error, does not throw
+bool fromHex(char character, uint8_t &value);
+
+// Assigns values of hex 'text' to buffer 'data' up to 'bufferSize',
+// returns actual data size, throws on error
+size_t fromHex(const std::string &text, void *data, size_t bufferSize);
+
+// Assigns values of hex 'text' to buffer 'data' up to 'bufferSize',
+// assigns actual data size to 'size', returns false on error, does not throw
+bool fromHex(const std::string &text, void *data, size_t bufferSize, size_t &size);
+
+// Returns values of hex 'text', throws on error
+std::vector<uint8_t> fromHex(const std::string &text);
+
+// Appends values of hex 'text' to 'data', returns false on error, does not throw
+bool fromHex(const std::string &text, std::vector<uint8_t> &data);
 
 template <typename T>
-bool podFromHex(const std::string& text, T& val) {
-  size_t outSize;
-  return fromHex(text, &val, sizeof(val), outSize) && outSize == sizeof(val);
+bool podFromHex(const std::string &text, T &val)
+{
+    size_t outSize;
+
+    return fromHex(text, &val, sizeof(val), outSize) && outSize == sizeof(val);
 }
 
-std::string toHex(const void* data, size_t size); // Returns hex representation of ('data', 'size'), does not throw
-void toHex(const void* data, size_t size, std::string& text); // Appends hex representation of ('data', 'size') to 'text', does not throw
-std::string toHex(const std::vector<uint8_t>& data); // Returns hex representation of 'data', does not throw
-void toHex(const std::vector<uint8_t>& data, std::string& text); // Appends hex representation of 'data' to 'text', does not throw
+// Returns hex representation of ('data', 'size'), does not throw
+std::string toHex(const void *data, size_t size);
+
+// Appends hex representation of ('data', 'size') to 'text', does not throw
+void toHex(const void *data, size_t size, std::string &text);
+
+// Returns hex representation of 'data', does not throw
+std::string toHex(const std::vector<uint8_t> &data);
+
+// Appends hex representation of 'data' to 'text', does not throw
+void toHex(const std::vector<uint8_t> &data, std::string &text);
 
 template<class T>
-std::string podToHex(const T& s) {
-  return toHex(&s, sizeof(s));
+std::string podToHex(const T &s)
+{
+    return toHex(&s, sizeof(s));
 }
 
-std::string extract(std::string& text, char delimiter); // Does not throw
-std::string extract(const std::string& text, char delimiter, size_t& offset); // Does not throw
+std::string extract(std::string &text, char delimiter);
+std::string extract(const std::string &text, char delimiter, size_t &offset);
 
-template<typename T> T fromString(const std::string& text) { // Throws on error
-  T value;
-  std::istringstream stream(text);
-  stream >> value;
-  if (stream.fail()) {
-    throw std::runtime_error("fromString: unable to parse value");
-  }
-
-  return value;
-}
-
-template<typename T> bool fromString(const std::string& text, T& value) { // Does not throw
-  std::istringstream stream(text);
-  stream >> value;
-  return !stream.fail();
-}
-
-template<typename T> std::vector<T> fromDelimitedString(const std::string& source, char delimiter) { // Throws on error
-  std::vector<T> data;
-  for (size_t offset = 0; offset != source.size();) {
-    data.emplace_back(fromString<T>(extract(source, delimiter, offset)));
-  }
-
-  return data;
-}
-
-template<typename T> bool fromDelimitedString(const std::string& source, char delimiter, std::vector<T>& data) { // Does not throw
-  for (size_t offset = 0; offset != source.size();) {
+template<typename T>
+T fromString(const std::string &text)
+{
     T value;
-    if (!fromString<T>(extract(source, delimiter, offset), value)) {
-      return false;
+
+    std::istringstream stream(text);
+    stream >> value;
+
+    if (stream.fail()) {
+        throw std::runtime_error("fromString: unable to parse value");
     }
 
-    data.emplace_back(value);
-  }
-
-  return true;
+    return value;
 }
 
-template<typename T> std::string toString(const T& value) { // Does not throw
-  std::ostringstream stream;
-  stream << value;
-  return stream.str();
+template<typename T>
+bool fromString(const std::string &text, T &value)
+{
+    std::istringstream stream(text);
+    stream >> value;
+
+    return !stream.fail();
 }
 
-template<typename T> void toString(const T& value, std::string& text) { // Does not throw
-  std::ostringstream stream;
-  stream << value;
-  text += stream.str();
+template<typename T>
+std::vector<T> fromDelimitedString(const std::string &source, char delimiter)
+{
+    std::vector<T> data;
+    for (size_t offset = 0; offset != source.size();) {
+        data.emplace_back(fromString<T>(extract(source, delimiter, offset)));
+    }
+
+    return data;
 }
 
-bool loadFileToString(const std::string& filepath, std::string& buf);
-bool saveStringToFile(const std::string& filepath, const std::string& buf);
+template<typename T>
+bool fromDelimitedString(const std::string &source, char delimiter, std::vector<T> &data)
+{
+    for (size_t offset = 0; offset != source.size();) {
+        T value;
+        if (!fromString<T>(extract(source, delimiter, offset), value)) {
+            return false;
+        }
 
+        data.emplace_back(value);
+    }
 
-std::string base64Decode(std::string const& encoded_string);
+    return true;
+}
+
+template<typename T>
+std::string toString(const T &value)
+{
+    std::ostringstream stream;
+    stream << value;
+
+    return stream.str();
+}
+
+template<typename T>
+void toString(const T &value, std::string &text)
+{
+    std::ostringstream stream;
+    stream << value;
+    text += stream.str();
+}
+
+bool loadFileToString(const std::string &filepath, std::string &buf);
+bool saveStringToFile(const std::string &filepath, const std::string &buf);
+
+std::string base64Decode(std::string const &encoded_string);
 
 std::string ipAddressToString(uint32_t ip);
-uint32_t stringToIpAddress(std::string addr);
-bool parseIpAddressAndPort(uint32_t& ip, uint32_t& port, const std::string& addr);
+uint32_t stringToIpAddress(const std::string &addr);
+bool parseIpAddressAndPort(uint32_t &ip, uint32_t &port, const std::string &addr);
 
 std::string timeIntervalToString(uint64_t intervalInSeconds);
 
-}
+} // namespace Common
