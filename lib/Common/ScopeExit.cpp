9272e11589aa0cd51bbe10a6fc78ec71b270a304
Alexander <speqtr@users.noreply.github.com>
2019-05-04 20:22:03 +0300

    Fixed code style issues in Common/ScopedExit

--- lib/Common/ScopeExit.cpp
+++ lib/Common/ScopeExit.cpp
@@ -16,23 +16,26 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with Qwertycoin.  If not, see <http://www.gnu.org/licenses/>.
 
-#include "ScopeExit.h"
+#include <Common/ScopeExit.h>
 
 namespace Tools {
 
-ScopeExit::ScopeExit(std::function<void()>&& handler) :
-  m_handler(std::move(handler)),
-  m_cancelled(false) {
+ScopeExit::ScopeExit(std::function<void()> &&handler)
+    : m_handler(std::move(handler)),
+      m_cancelled(false)
+{
 }
 
-ScopeExit::~ScopeExit() {
-  if (!m_cancelled) {
-    m_handler();
-  }
+ScopeExit::~ScopeExit()
+{
+    if (!m_cancelled) {
+        m_handler();
+    }
 }
 
-void ScopeExit::cancel() {
-  m_cancelled = true;
+void ScopeExit::cancel()
+{
+    m_cancelled = true;
 }
 
-}
+} // namespace Tools
