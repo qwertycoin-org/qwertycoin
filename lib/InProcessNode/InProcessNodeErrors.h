e9f63629d9e2e233d2f10720fb422eb789bb88a9
Alexander <speqtr@users.noreply.github.com>
2019-05-06 20:23:30 +0300

    Fixed code style issues in InProcessNode/InProcessNodeErrors

--- lib/InProcessNode/InProcessNodeErrors.h
+++ lib/InProcessNode/InProcessNodeErrors.h
@@ -22,49 +22,66 @@
 #include <system_error>
 
 namespace CryptoNote {
+
 namespace error {
 
-enum InProcessNodeErrorCodes {
-  NOT_INITIALIZED = 1,
-  ALREADY_INITIALIZED,
-  NETWORK_ERROR,
-  NODE_BUSY,
-  INTERNAL_NODE_ERROR,
-  REQUEST_ERROR
+enum InProcessNodeErrorCodes
+{
+    NOT_INITIALIZED = 1,
+    ALREADY_INITIALIZED,
+    NETWORK_ERROR,
+    NODE_BUSY,
+    INTERNAL_NODE_ERROR,
+    REQUEST_ERROR
 };
 
-class InProcessNodeErrorCategory : public std::error_category {
+class InProcessNodeErrorCategory : public std::error_category
+{
 public:
-  static InProcessNodeErrorCategory INSTANCE;
+    static InProcessNodeErrorCategory INSTANCE;
 
-  virtual const char* name() const throw() override {
-    return "InProcessNodeErrorCategory";
-  }
-
-  virtual std::error_condition default_error_condition(int ev) const throw() override {
-    return std::error_condition(ev, *this);
-  }
-
-  virtual std::string message(int ev) const override {
-    switch (ev) {
-      case NOT_INITIALIZED:     return "Object was not initialized";
-      case ALREADY_INITIALIZED: return "Object has been already initialized";
-      case NETWORK_ERROR:       return "Network error";
-      case NODE_BUSY:           return "Node is busy";
-      case INTERNAL_NODE_ERROR: return "Internal node error";
-      case REQUEST_ERROR:       return "Error in request parameters";
-      default:                  return "Unknown error";
+    const char *name() const noexcept override
+    {
+        return "InProcessNodeErrorCategory";
     }
-  }
+
+    std::error_condition default_error_condition(int ev) const noexcept override
+    {
+        return std::error_condition{ev, *this};
+    }
+
+    std::string message(int ev) const override
+    {
+        switch (ev) {
+        case NOT_INITIALIZED:
+            return "Object was not initialized";
+        case ALREADY_INITIALIZED:
+            return "Object has been already initialized";
+        case NETWORK_ERROR:
+            return "Network error";
+        case NODE_BUSY:
+            return "Node is busy";
+        case INTERNAL_NODE_ERROR:
+            return "Internal node error";
+        case REQUEST_ERROR:
+            return "Error in request parameters";
+        default:
+            return "Unknown error";
+        }
+    }
 
 private:
-  InProcessNodeErrorCategory() {
-  }
+    InProcessNodeErrorCategory() = default;
 };
 
-} //namespace error
-} //namespace CryptoNote
+} // namespace error
 
-inline std::error_code make_error_code(CryptoNote::error::InProcessNodeErrorCodes e) {
-  return std::error_code(static_cast<int>(e), CryptoNote::error::InProcessNodeErrorCategory::INSTANCE);
+} // namespace CryptoNote
+
+inline std::error_code make_error_code(CryptoNote::error::InProcessNodeErrorCodes e)
+{
+    return std::error_code{
+        static_cast<int>(e),
+        CryptoNote::error::InProcessNodeErrorCategory::INSTANCE
+    };
 }
