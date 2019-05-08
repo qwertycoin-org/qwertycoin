89ed284631a51947efa907ce8d61934a2645a93a
Alexander <speqtr@users.noreply.github.com>
2019-05-08 19:36:47 +0300

    Fixed code style issues in Http/HttpResponse

--- lib/Http/HttpResponse.cpp
+++ lib/Http/HttpResponse.cpp
@@ -17,88 +17,89 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with Qwertycoin.  If not, see <http://www.gnu.org/licenses/>.
 
-#include "HttpResponse.h"
-
 #include <stdexcept>
+#include <Http/HttpResponse.h>
 
 namespace {
 
-const char* getStatusString(CryptoNote::HttpResponse::HTTP_STATUS status) {
-  switch (status) {
-  case CryptoNote::HttpResponse::STATUS_200:
-    return "200 OK";
-  case CryptoNote::HttpResponse::STATUS_401:
-    return "401 Unauthorized";
-  case CryptoNote::HttpResponse::STATUS_404:
-    return "404 Not Found";
-  case CryptoNote::HttpResponse::STATUS_500:
-    return "500 Internal Server Error";
-  default:
-    throw std::runtime_error("Unknown HTTP status code is given");
-  }
-
-  return ""; //unaccessible
+const char *getStatusString(CryptoNote::HttpResponse::HTTP_STATUS status)
+{
+    switch (status) {
+    case CryptoNote::HttpResponse::STATUS_200:
+        return "200 OK";
+    case CryptoNote::HttpResponse::STATUS_401:
+        return "401 Unauthorized";
+    case CryptoNote::HttpResponse::STATUS_404:
+        return "404 Not Found";
+    case CryptoNote::HttpResponse::STATUS_500:
+        return "500 Internal Server Error";
+    default:
+        throw std::runtime_error("Unknown HTTP status code is given");
+    }
 }
 
-const char* getErrorBody(CryptoNote::HttpResponse::HTTP_STATUS status) {
-  switch (status) {
-  case CryptoNote::HttpResponse::STATUS_401:
-    return "Authorization required\n";
-  case CryptoNote::HttpResponse::STATUS_404:
-    return "Requested url is not found\n";
-  case CryptoNote::HttpResponse::STATUS_500:
-    return "Internal server error is occurred\n";
-  default:
-    throw std::runtime_error("Error body for given status is not available");
-  }
-
-  return ""; //unaccessible
+const char *getErrorBody(CryptoNote::HttpResponse::HTTP_STATUS status)
+{
+    switch (status) {
+    case CryptoNote::HttpResponse::STATUS_401:
+        return "Authorization required\n";
+    case CryptoNote::HttpResponse::STATUS_404:
+        return "Requested url is not found\n";
+    case CryptoNote::HttpResponse::STATUS_500:
+        return "Internal server error is occurred\n";
+    default:
+        throw std::runtime_error("Error body for given status is not available");
+    }
 }
 
-} //namespace
+} // namespace
 
 namespace CryptoNote {
 
-HttpResponse::HttpResponse() {
-  status = STATUS_200;
-  headers["Server"] = "CryptoNote-based HTTP server";
-  headers["Access-Control-Allow-Origin"] = "*";
+HttpResponse::HttpResponse()
+{
+    status = STATUS_200;
+    headers["Server"] = "CryptoNote-based HTTP server";
+    headers["Access-Control-Allow-Origin"] = "*";
 }
 
-void HttpResponse::setStatus(HTTP_STATUS s) {
-  status = s;
-
-  if (status != HttpResponse::STATUS_200) {
-    setBody(getErrorBody(status));
-  }
+void HttpResponse::setStatus(HTTP_STATUS s)
+{
+    status = s;
+    if (status != HttpResponse::STATUS_200) {
+        setBody(getErrorBody(status));
+    }
 }
 
-void HttpResponse::addHeader(const std::string& name, const std::string& value) {
-  headers[name] = value;
+void HttpResponse::addHeader(const std::string &name, const std::string &value)
+{
+    headers[name] = value;
 }
 
-void HttpResponse::setBody(const std::string& b) {
-  body = b;
-  if (!body.empty()) {
-    headers["Content-Length"] = std::to_string(body.size());
-  } else {
-    headers.erase("Content-Length");
-  }
+void HttpResponse::setBody(const std::string &b)
+{
+    body = b;
+    if (!body.empty()) {
+        headers["Content-Length"] = std::to_string(body.size());
+    } else {
+        headers.erase("Content-Length");
+    }
 }
 
-std::ostream& HttpResponse::printHttpResponse(std::ostream& os) const {
-  os << "HTTP/1.1 " << getStatusString(status) << "\r\n";
+std::ostream &HttpResponse::printHttpResponse(std::ostream &os) const
+{
+    os << "HTTP/1.1 " << getStatusString(status) << "\r\n";
 
-  for (auto pair: headers) {
-    os << pair.first << ": " << pair.second << "\r\n";
-  }
-  os << "\r\n";
+    for (const auto &pair: headers) {
+        os << pair.first << ": " << pair.second << "\r\n";
+    }
+    os << "\r\n";
 
-  if (!body.empty()) {
-    os << body;
-  }
+    if (!body.empty()) {
+        os << body;
+    }
 
-  return os;
+    return os;
 }
 
-} //namespace CryptoNote
+} // namespace CryptoNote
