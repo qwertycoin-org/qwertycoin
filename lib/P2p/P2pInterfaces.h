06f961d8a95658087f50452c68b4b22ff5af4bda
Alexander <speqtr@users.noreply.github.com>
2019-05-13 17:23:13 +0300

    Fixed code style issues in P2p/P2pInterfaces

--- lib/P2p/P2pInterfaces.h
+++ lib/P2p/P2pInterfaces.h
@@ -20,29 +20,31 @@
 
 #include <cstdint>
 #include <memory>
-
 #include <CryptoNote.h>
 
 namespace CryptoNote {
 
-struct P2pMessage {
-  uint32_t type;
-  BinaryArray data;
+struct P2pMessage
+{
+    uint32_t type;
+    BinaryArray data;
 };
 
-class IP2pConnection {
+class IP2pConnection
+{
 public:
-  virtual ~IP2pConnection();
-  virtual void read(P2pMessage &message) = 0;
-  virtual void write(const P2pMessage &message) = 0;
-  virtual void ban() = 0;
-  virtual void stop() = 0;
+    virtual ~IP2pConnection() = default;
+    virtual void read(P2pMessage &message) = 0;
+    virtual void write(const P2pMessage &message) = 0;
+    virtual void ban() = 0;
+    virtual void stop() = 0;
 };
 
-class IP2pNode {
+class IP2pNode
+{
 public:
-  virtual std::unique_ptr<IP2pConnection> receiveConnection() = 0;
-  virtual void stop() = 0;
+    virtual std::unique_ptr<IP2pConnection> receiveConnection() = 0;
+    virtual void stop() = 0;
 };
 
-}
+} // namespace CryptoNote
