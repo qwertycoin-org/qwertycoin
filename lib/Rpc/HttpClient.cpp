932560fa2af113e28b000f4397adfdbbd70b3610
Alexander <speqtr@users.noreply.github.com>
2019-05-15 15:33:57 +0300

    Fixed code style issues in lib/Rpc/HttpClient

--- lib/Rpc/HttpClient.cpp
+++ lib/Rpc/HttpClient.cpp
@@ -16,75 +16,85 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with Qwertycoin.  If not, see <http://www.gnu.org/licenses/>.
 
-#include "HttpClient.h"
-
 #include <Http/HttpParser.h>
 #include <System/Ipv4Resolver.h>
 #include <System/Ipv4Address.h>
 #include <System/TcpConnector.h>
+#include <Rpc/HttpClient.h>
 
 namespace CryptoNote {
 
-HttpClient::HttpClient(System::Dispatcher& dispatcher, const std::string& address, uint16_t port) :
-  m_dispatcher(dispatcher), m_address(address), m_port(port) {
+HttpClient::HttpClient(System::Dispatcher &dispatcher, const std::string &address, uint16_t port)
+    : m_dispatcher(dispatcher),
+      m_address(address),
+      m_port(port)
+{
 }
 
-HttpClient::~HttpClient() {
-  if (m_connected) {
-    disconnect();
-  }
+HttpClient::~HttpClient()
+{
+    if (m_connected) {
+        disconnect();
+    }
 }
 
-void HttpClient::request(const HttpRequest &req, HttpResponse &res) {
-  if (!m_connected) {
-    connect();
-  }
+void HttpClient::request(const HttpRequest &req, HttpResponse &res)
+{
+    if (!m_connected) {
+        connect();
+    }
 
-  try {
-    std::iostream stream(m_streamBuf.get());
-    HttpParser parser;
-    stream << req;
-    stream.flush();
-    parser.receiveResponse(stream, res);
-  } catch (const std::exception &) {
-    disconnect();
-    throw;
-  }
+    try {
+        std::iostream stream(m_streamBuf.get());
+        HttpParser parser;
+        stream << req;
+        stream.flush();
+        parser.receiveResponse(stream, res);
+    } catch (const std::exception &) {
+        disconnect();
+        throw;
+    }
 }
 
-void HttpClient::connect() {
-  try {
-    auto ipAddr = System::Ipv4Resolver(m_dispatcher).resolve(m_address);
-    m_connection = System::TcpConnector(m_dispatcher).connect(ipAddr, m_port);
-    m_streamBuf.reset(new System::TcpStreambuf(m_connection));
-    m_connected = true;
-  } catch (const std::exception& e) {
-    throw ConnectException(e.what());
-  }
+void HttpClient::connect()
+{
+    try {
+        auto ipAddr = System::Ipv4Resolver(m_dispatcher).resolve(m_address);
+        m_connection = System::TcpConnector(m_dispatcher).connect(ipAddr, m_port);
+        m_streamBuf.reset(new System::TcpStreambuf(m_connection));
+        m_connected = true;
+    } catch (const std::exception &e) {
+        throw ConnectException(e.what());
+    }
 }
 
-bool HttpClient::isConnected() const {
-  return m_connected;
+bool HttpClient::isConnected() const
+{
+    return m_connected;
 }
 
-void HttpClient::disconnect() {
-  m_streamBuf.reset();
-  try {
-    m_connection.write(nullptr, 0); //Socket shutdown.
-  } catch (std::exception&) {
-    //Ignoring possible exception.
-  }
+void HttpClient::disconnect()
+{
+    m_streamBuf.reset();
 
-  try {
-    m_connection = System::TcpConnection();
-  } catch (std::exception&) {
-    //Ignoring possible exception.
-  }
+    try {
+        m_connection.write(nullptr, 0); // socket shutdown.
+    } catch (std::exception &) {
+        // ignoring possible exception.
+    }
 
-  m_connected = false;
+    try {
+        m_connection = System::TcpConnection();
+    } catch (std::exception &) {
+        // ignoring possible exception.
+    }
+
+    m_connected = false;
 }
 
-ConnectException::ConnectException(const std::string& whatArg) : std::runtime_error(whatArg.c_str()) {
+ConnectException::ConnectException(const std::string &whatArg)
+    : std::runtime_error(whatArg.c_str())
+{
 }
 
-}
+} // namespace CryptoNote
