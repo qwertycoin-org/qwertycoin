91fb0f9bc2066a3fbc496567a1963ca7158c4c4c
Alexander <speqtr@users.noreply.github.com>
2019-05-17 20:33:00 +0300

    Fixed code style issues in lib/System/RemoteEventLock

--- lib/System/RemoteEventLock.h
+++ lib/System/RemoteEventLock.h
@@ -23,14 +23,15 @@ namespace System {
 class Dispatcher;
 class Event;
 
-class RemoteEventLock {
+class RemoteEventLock
+{
 public:
-  RemoteEventLock(Dispatcher& dispatcher, Event& event);
-  ~RemoteEventLock();
+    RemoteEventLock(Dispatcher &dispatcher, Event &event);
+    ~RemoteEventLock();
 
 private:
-  Dispatcher& dispatcher;
-  Event& event;
+    Dispatcher &dispatcher;
+    Event &event;
 };
 
-}
+} // namespace System
