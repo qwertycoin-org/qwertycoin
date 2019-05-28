0bf38ecaeb4658dee551f807a5dc464ee16aabbd
Alexander <speqtr@users.noreply.github.com>
2019-05-28 18:31:35 +0300

    Fixed code style issues in lib/WalletLegacy/WalletUtils

--- lib/WalletLegacy/WalletUtils.h
+++ lib/WalletLegacy/WalletUtils.h
@@ -21,29 +21,31 @@
 #include <exception>
 #include <iomanip>
 #include <iostream>
-
-#include "IWalletLegacy.h"
-#include "Wallet/WalletErrors.h"
+#include <Wallet/WalletErrors.h>
+#include <WalletLegacy/IWalletLegacy.h>
 
 namespace CryptoNote {
 
 inline void throwIf(bool expr, CryptoNote::error::WalletErrorCodes ec)
 {
-  if (expr)
-    throw std::system_error(make_error_code(ec));
+    if (expr) {
+        throw std::system_error(make_error_code(ec));
+    }
 }
 
-inline std::ostream& operator <<(std::ostream& ostr, const Crypto::Hash& hash) {
-  std::ios_base::fmtflags flags = ostr.setf(std::ios_base::hex, std::ios_base::basefield);
-  char fill = ostr.fill('0');
+inline std::ostream &operator<<(std::ostream &ostr, const Crypto::Hash &hash)
+{
+    std::ios_base::fmtflags flags = ostr.setf(std::ios_base::hex, std::ios_base::basefield);
+    char fill = ostr.fill('0');
 
-  for (auto b : hash.data) {
-    ostr << std::setw(2) << static_cast<unsigned int>(b);
-  }
+    for (auto b : hash.data) {
+        ostr << std::setw(2) << static_cast<unsigned int>(b);
+    }
 
-  ostr.fill(fill);
-  ostr.setf(flags);
-  return ostr;
+    ostr.fill(fill);
+    ostr.setf(flags);
+
+    return ostr;
 }
 
-} //namespace CryptoNote
+} // namespace CryptoNote
