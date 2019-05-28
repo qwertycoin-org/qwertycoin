83ecb9a53b6db1d4015bf0fd7be3979cbdc1377d
Alexander <speqtr@users.noreply.github.com>
2019-05-28 18:31:22 +0300

    Fixed code style issues in lib/WalletLegacy/WalletSendTransactionContext.h

--- lib/WalletLegacy/WalletSendTransactionContext.h
+++ lib/WalletLegacy/WalletSendTransactionContext.h
@@ -21,37 +21,43 @@
 
 #include <list>
 #include <vector>
-
-#include "CryptoNoteCore/CryptoNoteBasic.h"
-#include "IWalletLegacy.h"
-#include "ITransfersContainer.h"
-#include "CryptoNoteCore/CryptoNoteFormatUtils.h"
+#include <CryptoNoteCore/CryptoNoteBasic.h>
+#include <CryptoNoteCore/CryptoNoteFormatUtils.h>
+#include <WalletLegacy/IWalletLegacy.h>
+#include <ITransfersContainer.h>
 
 namespace CryptoNote {
 
 struct TxDustPolicy
 {
-  uint64_t dustThreshold;
-  bool addToFee;
-  CryptoNote::AccountPublicAddress addrForDust;
+    explicit TxDustPolicy(
+        uint64_t a_dust_threshold = 0,
+        bool an_add_to_fee = true,
+        CryptoNote::AccountPublicAddress
+        an_addr_for_dust = CryptoNote::AccountPublicAddress())
+        : dustThreshold(a_dust_threshold),
+          addToFee(an_add_to_fee),
+          addrForDust(an_addr_for_dust)
+    {
+    }
 
-  TxDustPolicy(uint64_t a_dust_threshold = 0, bool an_add_to_fee = true, CryptoNote::AccountPublicAddress an_addr_for_dust = CryptoNote::AccountPublicAddress())
-    : dustThreshold(a_dust_threshold), addToFee(an_add_to_fee), addrForDust(an_addr_for_dust) {}
+    uint64_t dustThreshold;
+    bool addToFee;
+    CryptoNote::AccountPublicAddress addrForDust;
 };
 
 struct SendTransactionContext
 {
-  TransactionId transactionId;
-  std::vector<CryptoNote::COMMAND_RPC_GET_RANDOM_OUTPUTS_FOR_AMOUNTS::outs_for_amount> outs;
-  uint64_t foundMoney;
-  std::list<TransactionOutputInformation> selectedTransfers;
-  TxDustPolicy dustPolicy;
-  uint64_t mixIn;
-  std::vector<tx_message_entry> messages;
-  uint64_t ttl;
-  std::string sender;
-  Crypto::SecretKey tx_key = NULL_SECRET_KEY;
+    TransactionId transactionId;
+    std::vector<CryptoNote::COMMAND_RPC_GET_RANDOM_OUTPUTS_FOR_AMOUNTS::outs_for_amount> outs;
+    uint64_t foundMoney;
+    std::list<TransactionOutputInformation> selectedTransfers;
+    TxDustPolicy dustPolicy;
+    uint64_t mixIn;
+    std::vector<tx_message_entry> messages;
+    uint64_t ttl;
+    std::string sender;
+    Crypto::SecretKey tx_key = NULL_SECRET_KEY;
 };
 
-
-} //namespace CryptoNote
+} // namespace CryptoNote
