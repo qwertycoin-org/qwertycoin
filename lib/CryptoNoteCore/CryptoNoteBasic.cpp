3d8363709788676b9281f0d1becef664571f0ec4
Alexander <speqtr@users.noreply.github.com>
2019-06-10 18:58:09 +0300

    Fixed code style issues in lib/CryptoNoteCore/CryptoNoteBasic

--- lib/CryptoNoteCore/CryptoNoteBasic.cpp
+++ lib/CryptoNoteCore/CryptoNoteBasic.cpp
@@ -16,15 +16,18 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with Qwertycoin.  If not, see <http://www.gnu.org/licenses/>.
 
-#include "CryptoNoteBasic.h"
-#include "crypto/crypto.h"
+#include <crypto/crypto.h>
+#include <CryptoNoteCore/CryptoNoteBasic.h>
 
 namespace CryptoNote {
 
-KeyPair generateKeyPair() {
-  KeyPair k;
-  Crypto::generate_keys(k.publicKey, k.secretKey);
-  return k;
+KeyPair generateKeyPair()
+{
+    KeyPair k;
+
+    Crypto::generate_keys(k.publicKey, k.secretKey);
+
+    return k;
 }
 
-}
+} // namespace CryptoNote
