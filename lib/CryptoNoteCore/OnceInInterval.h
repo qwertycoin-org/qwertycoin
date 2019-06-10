0cbe588fe577826476410da407debd3d5b525bd9
Alexander <speqtr@users.noreply.github.com>
2019-06-10 20:27:52 +0300

    Fixed code style issues in lib/CryptoNoteCore/OnceInInterval.h

--- lib/CryptoNoteCore/OnceInInterval.h
+++ lib/CryptoNoteCore/OnceInInterval.h
@@ -20,31 +20,34 @@
 
 #include <time.h>
 
-namespace CryptoNote
+namespace CryptoNote {
+
+class OnceInInterval
 {
-
-class OnceInInterval {
 public:
-
-  OnceInInterval(unsigned interval, bool startNow = true)
-    : m_interval(interval), m_lastCalled(startNow ? 0 : time(nullptr)) {}
-
-  template<class F>
-  bool call(F func) {
-    time_t currentTime = time(nullptr);
-
-    if (currentTime - m_lastCalled > m_interval) {
-      bool res = func();
-      time(&m_lastCalled);
-      return res;
+    explicit OnceInInterval(unsigned interval, bool startNow = true)
+        : m_interval(interval),
+          m_lastCalled(startNow ? 0 : time(nullptr))
+    {
     }
 
-    return true;
-  }
+    template<class F>
+    bool call(F func)
+    {
+        time_t currentTime = time(nullptr);
+
+        if (currentTime - m_lastCalled > m_interval) {
+            bool res = func();
+            time(&m_lastCalled);
+            return res;
+        }
+
+        return true;
+    }
 
 private:
-  time_t m_lastCalled;
-  time_t m_interval;
+    time_t m_lastCalled;
+    time_t m_interval;
 };
 
-}
+} // namespace CryptoNote
