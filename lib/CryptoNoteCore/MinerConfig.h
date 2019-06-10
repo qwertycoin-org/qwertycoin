934c2dd6ed7e946ce8937776cfe24b687639c88e
Alexander <speqtr@users.noreply.github.com>
2019-06-10 20:27:41 +0300

    Fixed code style issues in lib/CryptoNoteCore/MinerConfig

--- lib/CryptoNoteCore/MinerConfig.h
+++ lib/CryptoNoteCore/MinerConfig.h
@@ -20,21 +20,21 @@
 
 #include <cstdint>
 #include <string>
-
 #include <boost/program_options.hpp>
 
 namespace CryptoNote {
 
-class MinerConfig {
+class MinerConfig
+{
 public:
-  MinerConfig();
+    MinerConfig();
 
-  static void initOptions(boost::program_options::options_description& desc);
-  void init(const boost::program_options::variables_map& options);
+    static void initOptions(boost::program_options::options_description &desc);
+    void init(const boost::program_options::variables_map &options);
 
-  std::string extraMessages;
-  std::string startMining;
-  uint32_t miningThreads;
+    std::string extraMessages;
+    std::string startMining;
+    uint32_t miningThreads;
 };
 
-} //namespace CryptoNote
+} // namespace CryptoNote
