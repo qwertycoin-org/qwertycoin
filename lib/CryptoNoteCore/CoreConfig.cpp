5d66c54c59489b0e410d4465503978a1d90cccbb
Alexander <speqtr@users.noreply.github.com>
2019-06-10 18:57:42 +0300

    Fixed code style issues in lib/CryptoNoteCore/CoreConfig

--- lib/CryptoNoteCore/CoreConfig.cpp
+++ lib/CryptoNoteCore/CoreConfig.cpp
@@ -16,24 +16,30 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with Qwertycoin.  If not, see <http://www.gnu.org/licenses/>.
 
-#include "CoreConfig.h"
-
-#include "Common/Util.h"
-#include "Common/CommandLine.h"
+#include <Common/CommandLine.h>
+#include <Common/Util.h>
+#include <CryptoNoteCore/CoreConfig.h>
 
 namespace CryptoNote {
 
-CoreConfig::CoreConfig() {
-  configFolder = Tools::getDefaultDataDirectory();
+CoreConfig::CoreConfig()
+{
+    configFolder = Tools::getDefaultDataDirectory();
 }
 
-void CoreConfig::init(const boost::program_options::variables_map& options) {
-  if (options.count(command_line::arg_data_dir.name) != 0 && (!options[command_line::arg_data_dir.name].defaulted() || configFolder == Tools::getDefaultDataDirectory())) {
-    configFolder = command_line::get_arg(options, command_line::arg_data_dir);
-    configFolderDefaulted = options[command_line::arg_data_dir.name].defaulted();
-  }
+void CoreConfig::init(const boost::program_options::variables_map &options)
+{
+    if (options.count(command_line::arg_data_dir.name) != 0
+        && (!options[command_line::arg_data_dir.name].defaulted()
+            || configFolder == Tools::getDefaultDataDirectory())
+        ) {
+        configFolder = command_line::get_arg(options, command_line::arg_data_dir);
+        configFolderDefaulted = options[command_line::arg_data_dir.name].defaulted();
+    }
 }
 
-void CoreConfig::initOptions(boost::program_options::options_description& desc) {
+void CoreConfig::initOptions(boost::program_options::options_description &desc)
+{
 }
-} //namespace CryptoNote
+
+} // namespace CryptoNote
--- lib/CryptoNoteCore/CoreConfig.h
+++ lib/CryptoNoteCore/CoreConfig.h
@@ -19,20 +19,20 @@
 #pragma once
 
 #include <string>
-
 #include <boost/program_options.hpp>
 
 namespace CryptoNote {
 
-class CoreConfig {
+class CoreConfig
+{
 public:
-  CoreConfig();
+    CoreConfig();
 
-  static void initOptions(boost::program_options::options_description& desc);
-  void init(const boost::program_options::variables_map& options);
+    static void initOptions(boost::program_options::options_description &desc);
+    void init(const boost::program_options::variables_map &options);
 
-  std::string configFolder;
-  bool configFolderDefaulted = true;
+    std::string configFolder;
+    bool configFolderDefaulted = true;
 };
 
-} //namespace CryptoNote
+} // namespace CryptoNote
