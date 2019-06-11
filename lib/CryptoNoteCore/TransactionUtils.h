9d4c0ace497e61c7e66164732dc31f918c83752e
Alexander <speqtr@users.noreply.github.com>
2019-06-11 19:46:24 +0300

    Fixed code style issues in lib/CryptoNoteCore/TransactionUtils

--- lib/CryptoNoteCore/TransactionUtils.h
+++ lib/CryptoNoteCore/TransactionUtils.h
@@ -16,28 +16,42 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with Qwertycoin.  If not, see <http://www.gnu.org/licenses/>.
 
-#include "CryptoNoteCore/CryptoNoteBasic.h"
-#include "ITransaction.h"
+#include <CryptoNoteCore/ITransaction.h>
+#include <CryptoNoteCore/CryptoNoteBasic.h>
 
 namespace CryptoNote {
 
-bool checkInputsKeyimagesDiff(const CryptoNote::TransactionPrefix& tx);
+bool checkInputsKeyimagesDiff(const CryptoNote::TransactionPrefix &tx);
 
 // TransactionInput helper functions
-size_t getRequiredSignaturesCount(const TransactionInput& in);
-uint64_t getTransactionInputAmount(const TransactionInput& in);
-TransactionTypes::InputType getTransactionInputType(const TransactionInput& in);
-const TransactionInput& getInputChecked(const CryptoNote::TransactionPrefix& transaction, size_t index);
-const TransactionInput& getInputChecked(const CryptoNote::TransactionPrefix& transaction, size_t index, TransactionTypes::InputType type);
+size_t getRequiredSignaturesCount(const TransactionInput &in);
+uint64_t getTransactionInputAmount(const TransactionInput &in);
+TransactionTypes::InputType getTransactionInputType(const TransactionInput &in);
+const TransactionInput &getInputChecked(const TransactionPrefix &transaction, size_t index);
+const TransactionInput &getInputChecked(
+    const TransactionPrefix &transaction,
+    size_t index,
+    TransactionTypes::InputType type);
 
-bool isOutToKey(const Crypto::PublicKey& spendPublicKey, const Crypto::PublicKey& outKey, const Crypto::KeyDerivation& derivation, size_t keyIndex);
+bool isOutToKey(
+    const Crypto::PublicKey &spendPublicKey,
+    const Crypto::PublicKey &outKey,
+    const Crypto::KeyDerivation &derivation,
+    size_t keyIndex);
 
 // TransactionOutput helper functions
-TransactionTypes::OutputType getTransactionOutputType(const TransactionOutputTarget& out);
-const TransactionOutput& getOutputChecked(const CryptoNote::TransactionPrefix& transaction, size_t index);
-const TransactionOutput& getOutputChecked(const CryptoNote::TransactionPrefix& transaction, size_t index, TransactionTypes::OutputType type);
+TransactionTypes::OutputType getTransactionOutputType(const TransactionOutputTarget &out);
+const TransactionOutput &getOutputChecked(const TransactionPrefix &transaction, size_t index);
+const TransactionOutput &getOutputChecked(
+    const TransactionPrefix &transaction,
+    size_t index,
+    TransactionTypes::OutputType type);
 
-bool findOutputsToAccount(const CryptoNote::TransactionPrefix& transaction, const AccountPublicAddress& addr,
-        const Crypto::SecretKey& viewSecretKey, std::vector<uint32_t>& out, uint64_t& amount);
+bool findOutputsToAccount(
+    const CryptoNote::TransactionPrefix &transaction,
+    const AccountPublicAddress &addr,
+    const Crypto::SecretKey &viewSecretKey,
+    std::vector<uint32_t> &out,
+    uint64_t &amount);
 
-} //namespace CryptoNote
+} // namespace CryptoNote
