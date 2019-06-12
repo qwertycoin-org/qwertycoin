dd6811de7e28b33ef1580712cd1b53008cc59dc4
Alexander <speqtr@users.noreply.github.com>
2019-06-12 15:37:43 +0300

    Fixed code style issues in lib/CryptoNoteCore/TransactionApi

--- lib/CryptoNoteCore/TransactionApiExtra.h
+++ lib/CryptoNoteCore/TransactionApiExtra.h
@@ -18,74 +18,93 @@
 
 #pragma once
 
-#include "CryptoNoteFormatUtils.h"
-#include "TransactionExtra.h"
+#include <CryptoNoteCore/CryptoNoteFormatUtils.h>
+#include <CryptoNoteCore/TransactionExtra.h>
 
 namespace CryptoNote {
 
-  class TransactionExtra {
-  public:
-    TransactionExtra() {}
-    TransactionExtra(const std::vector<uint8_t>& extra) {
-      parse(extra);
+class TransactionExtra
+{
+public:
+    TransactionExtra() = default;
+    TransactionExtra(const std::vector<uint8_t> &extra)
+    {
+        parse(extra);
     }
 
-    bool parse(const std::vector<uint8_t>& extra) {
-      fields.clear();
-      return CryptoNote::parseTransactionExtra(extra, fields);
-    }
-
-    template <typename T>
-    bool get(T& value) const {
-      auto it = find(typeid(T));
-      if (it == fields.end()) {
-        return false;
-      }
-      value = boost::get<T>(*it);
-      return true;
+    bool parse(const std::vector<uint8_t> &extra)
+    {
+        fields.clear();
+        return CryptoNote::parseTransactionExtra(extra, fields);
     }
 
     template <typename T>
-    void set(const T& value) {
-      auto it = find(typeid(T));
-      if (it != fields.end()) {
-        *it = value;
-      } else {
+    bool get(T &value) const
+    {
+        auto it = find(typeid(T));
+        if (it == fields.end()) {
+            return false;
+        }
+
+        value = boost::get<T>(*it);
+
+        return true;
+    }
+
+    template <typename T>
+    void set(const T &value)
+    {
+        auto it = find(typeid(T));
+        if (it != fields.end()) {
+            *it = value;
+        } else {
+            fields.push_back(value);
+        }
+    }
+
+    template <typename T>
+    void append(const T &value)
+    {
         fields.push_back(value);
-      }
     }
 
-    template <typename T>
-    void append(const T& value) {
-      fields.push_back(value);
+    bool getPublicKey(Crypto::PublicKey &pk) const
+    {
+        CryptoNote::TransactionExtraPublicKey extraPk;
+        if (!get(extraPk)) {
+            return false;
+        }
+
+        pk = extraPk.publicKey;
+
+        return true;
     }
 
-    bool getPublicKey(Crypto::PublicKey& pk) const {
-      CryptoNote::TransactionExtraPublicKey extraPk;
-      if (!get(extraPk)) {
-        return false;
-      }
-      pk = extraPk.publicKey;
-      return true;
+    std::vector<uint8_t> serialize() const
+    {
+        std::vector<uint8_t> extra;
+        writeTransactionExtra(extra, fields);
+
+        return extra;
     }
 
-    std::vector<uint8_t> serialize() const {
-      std::vector<uint8_t> extra;
-      writeTransactionExtra(extra, fields);
-      return extra;
+private:
+    std::vector<CryptoNote::TransactionExtraField>::const_iterator find(const std::type_info &t) const
+    {
+        return std::find_if(fields.begin(), fields.end(), [&t](const TransactionExtraField& f) {
+            return t == f.type();
+        });
     }
 
-  private:
-
-    std::vector<CryptoNote::TransactionExtraField>::const_iterator find(const std::type_info& t) const {
-      return std::find_if(fields.begin(), fields.end(), [&t](const CryptoNote::TransactionExtraField& f) { return t == f.type(); });
+    std::vector<CryptoNote::TransactionExtraField>::iterator find(const std::type_info &t)
+    {
+        return std::find_if(fields.begin(), fields.end(), [&t](const TransactionExtraField& f) {
+            return t == f.type();
+        });
     }
 
-    std::vector<CryptoNote::TransactionExtraField>::iterator find(const std::type_info& t) {
-      return std::find_if(fields.begin(), fields.end(), [&t](const CryptoNote::TransactionExtraField& f) { return t == f.type(); });
-    }
-
+private:
     std::vector<CryptoNote::TransactionExtraField> fields;
-  };
+};
 
-}
+} // namespace CryptoNote
