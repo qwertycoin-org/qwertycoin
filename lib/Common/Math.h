47b32bc953a5c812e8ee67782febf40bc60a2bfa
nnian <48366474+nnian@users.noreply.github.com>
2020-09-13 20:24:42 +0200

    minor codestyle fixes

--- lib/Common/Math.h
+++ lib/Common/Math.h
@@ -1,4 +1,5 @@
 // Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+// Copyright (c) 2020, The Karbowanec developers
 // Copyright (c) 2018-2020, The Qwertycoin Group.
 //
 // This file is part of Qwertycoin.
