adecb7c19c77553607ebda8c5a0f63c2eaf554f7
ExploShot <exploshot@qwertycoin.org>
2020-09-26 11:51:22 +0200

    Walletd Authorization Fix

--- lib/Common/StringTools.h
+++ lib/Common/StringTools.h
@@ -150,6 +150,7 @@ void toString(const T &value, std::string &text)
 bool loadFileToString(const std::string &filepath, std::string &buf);
 bool saveStringToFile(const std::string &filepath, const std::string &buf);
 
+std::string base64Encode(std::string const &encodableString);
 std::string base64Decode(std::string const &encoded_string);
 
 std::string ipAddressToString(uint32_t ip);
