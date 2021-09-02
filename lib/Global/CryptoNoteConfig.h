f54eb20307f9db89f93c255d6181ea6b2d36fcd9
Sw0rdf1sh1 <6624930+Sw0rdf1sh1@users.noreply.github.com>
2021-09-02 21:50:26 +0200

    add temporary testnet settings

--- lib/Global/CryptoNoteConfig.h
+++ lib/Global/CryptoNoteConfig.h
@@ -167,7 +167,7 @@ const uint32_t GOVERNANCE_HEIGHT_END                         = 4000000;
 
 const char     CRYPTONOTE_NAME[]                             = "Qwertycoin";
 const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff07029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101eddf1e272c1ffa70f49ca4eaad918578bc3b59689e53e48a1bc670fbdea08478";
-const char     DNS_CHECKPOINTS_HOST[]                        = "checkpoints.qwertycoin.org";
+const char     DNS_CHECKPOINTS_HOST[]                        = "che.qwertycoin.org";
 
 const uint8_t  TRANSACTION_VERSION_1                         =  1;
 const uint8_t  TRANSACTION_VERSION_2                         =  2;
@@ -206,8 +206,8 @@ const uint8_t  P2P_MINIMUM_VERSION                           = 6;
 const uint8_t  P2P_UPGRADE_WINDOW                            = 1;
 
 const size_t   P2P_CONNECTION_MAX_WRITE_BUFFER_SIZE          = 64 * 1024 * 1024; // 64 MB
-const uint32_t P2P_DEFAULT_CONNECTIONS_COUNT                 = 2;
-const size_t   P2P_DEFAULT_ANCHOR_CONNECTIONS_COUNT          = 2;
+const uint32_t P2P_DEFAULT_CONNECTIONS_COUNT                 = 1;
+const size_t   P2P_DEFAULT_ANCHOR_CONNECTIONS_COUNT          = 1;
 const size_t   P2P_DEFAULT_WHITELIST_CONNECTIONS_PERCENT     = 70;
 const uint32_t P2P_DEFAULT_HANDSHAKE_INTERVAL                = 60;            // seconds
 const uint32_t P2P_DEFAULT_PACKET_MAX_SIZE                   = 50000000;      // 50000000 bytes maximum packet size
@@ -221,7 +221,7 @@ const uint32_t P2P_FAILED_ADDR_FORGET_SECONDS                = (60 * 60);     //
 const uint32_t P2P_IP_BLOCKTIME                              = (60 * 60 * 24);//24 hour
 const uint32_t P2P_IP_FAILS_BEFORE_BLOCK                     = 10;
 const uint32_t P2P_IDLE_CONNECTION_KILL_INTERVAL             = (5 * 60);      //5 minutes
-const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "deaddeadbeef04d37a9499c67ccb730dc4734950f414cdb332b28c5ce764beaf";
+const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "ab361f81d80074df8b22d1a78c669f7d973ff525a8ffd64f35c6f39ccb347b06";
 
 const char        LATEST_VERSION_URL[]                       = "https://releases.qwertycoin.org";
 const std::string LICENSE_URL                                = "https://github.com/qwertycoin-org/qwertycoin/blob/master/LICENSE";
@@ -238,15 +238,7 @@ const std::string GOVERNANCE_VIEW_SECRET_KEY                 = "d6c03cec7de78fe3
 
 const char *const SEED_NODES[] = {
     "78.47.85.215:8196",
-    "148.251.115.235:8196",
-    "148.251.115.233:8196",
-    "node-00.qwertycoin.org:8196",
-    "node-01.qwertycoin.org:8196",
-    "node-02.qwertycoin.org:8196",
-    "node-03.qwertycoin.org:8196",
-    "node-04.qwertycoin.org:8196",
-    "node-06.qwertycoin.org:8196",
-    "node-07.qwertycoin.org:8196"
+    "116.203.84.161:8196"
 };
 
 const char *const BANNED_NODES[] = {
