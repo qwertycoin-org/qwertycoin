749d287f84c22cf0dc42f6809172f8f8c41c6cce
Sw0rdf1sh1 <6624930+Sw0rdf1sh1@users.noreply.github.com>
2021-09-04 23:28:08 +0200

    lower version to 6.0.7

--- lib/Global/CryptoNoteConfig.h
+++ lib/Global/CryptoNoteConfig.h
@@ -198,7 +198,7 @@ const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;
 // and the minimum version for communication between nodes
 const uint8_t  P2P_VERSION_1                                 = 1;
 const uint8_t  P2P_VERSION_2                                 = 2;
-const uint8_t  P2P_CURRENT_VERSION                           = 7;
+const uint8_t  P2P_CURRENT_VERSION                           = 6;
 const uint8_t  P2P_MINIMUM_VERSION                           = 6;
 
 // This defines the number of versions ahead we must see peers before we start displaying
