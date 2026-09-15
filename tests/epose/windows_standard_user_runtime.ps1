3b53223f0fdca2c84a8a5be9dd6746b50287c223
Alex <48366474+nnian@users.noreply.github.com>
2026-09-15 18:10:14 +0200

    ci(epose): test with restricted Windows token

--- tests/epose/windows_standard_user_runtime.ps1
+++ tests/epose/windows_standard_user_runtime.ps1
@@ -19,105 +19,205 @@ Add-Type -TypeDefinition @'
 using System;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
-using System.Security.Principal;
+using System.Text;
 
-public static class QwcLsaRights
+public static class QwcRestrictedProcess
 {
     [StructLayout(LayoutKind.Sequential)]
-    private struct LSA_OBJECT_ATTRIBUTES
+    private struct SID_AND_ATTRIBUTES
     {
-        public int Length;
-        public IntPtr RootDirectory;
-        public IntPtr ObjectName;
+        public IntPtr Sid;
         public uint Attributes;
-        public IntPtr SecurityDescriptor;
-        public IntPtr SecurityQualityOfService;
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    private struct STARTUPINFO
+    {
+        public int cb;
+        public string lpReserved;
+        public string lpDesktop;
+        public string lpTitle;
+        public uint dwX;
+        public uint dwY;
+        public uint dwXSize;
+        public uint dwYSize;
+        public uint dwXCountChars;
+        public uint dwYCountChars;
+        public uint dwFillAttribute;
+        public uint dwFlags;
+        public short wShowWindow;
+        public short cbReserved2;
+        public IntPtr lpReserved2;
+        public IntPtr hStdInput;
+        public IntPtr hStdOutput;
+        public IntPtr hStdError;
     }
 
     [StructLayout(LayoutKind.Sequential)]
-    private struct LSA_UNICODE_STRING
+    private struct PROCESS_INFORMATION
     {
-        public ushort Length;
-        public ushort MaximumLength;
-        public IntPtr Buffer;
+        public IntPtr hProcess;
+        public IntPtr hThread;
+        public uint dwProcessId;
+        public uint dwThreadId;
     }
 
-    [DllImport("advapi32.dll")]
-    private static extern uint LsaOpenPolicy(
-        IntPtr systemName,
-        ref LSA_OBJECT_ATTRIBUTES objectAttributes,
+    [DllImport("kernel32.dll")]
+    private static extern IntPtr GetCurrentProcess();
+
+    [DllImport("advapi32.dll", SetLastError = true)]
+    private static extern bool OpenProcessToken(
+        IntPtr processHandle,
         uint desiredAccess,
-        out IntPtr policyHandle);
+        out IntPtr tokenHandle);
 
-    [DllImport("advapi32.dll")]
-    private static extern uint LsaAddAccountRights(
-        IntPtr policyHandle,
-        IntPtr accountSid,
-        LSA_UNICODE_STRING[] userRights,
-        uint countOfRights);
+    [DllImport("advapi32.dll", SetLastError = true)]
+    private static extern bool CreateRestrictedToken(
+        IntPtr existingTokenHandle,
+        uint flags,
+        uint disableSidCount,
+        [In] SID_AND_ATTRIBUTES[] sidsToDisable,
+        uint deletePrivilegeCount,
+        IntPtr privilegesToDelete,
+        uint restrictedSidCount,
+        IntPtr sidsToRestrict,
+        out IntPtr newTokenHandle);
 
-    [DllImport("advapi32.dll")]
-    private static extern uint LsaRemoveAccountRights(
-        IntPtr policyHandle,
-        IntPtr accountSid,
-        [MarshalAs(UnmanagedType.U1)]
-        bool allRights,
-        LSA_UNICODE_STRING[] userRights,
-        uint countOfRights);
+    [DllImport("advapi32.dll", SetLastError = true)]
+    private static extern bool IsTokenRestricted(IntPtr tokenHandle);
 
-    [DllImport("advapi32.dll")]
-    private static extern uint LsaNtStatusToWinError(uint status);
+    [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+    private static extern bool CreateProcessAsUserW(
+        IntPtr token,
+        string applicationName,
+        StringBuilder commandLine,
+        IntPtr processAttributes,
+        IntPtr threadAttributes,
+        bool inheritHandles,
+        uint creationFlags,
+        IntPtr environment,
+        string currentDirectory,
+        ref STARTUPINFO startupInfo,
+        out PROCESS_INFORMATION processInformation);
 
-    [DllImport("advapi32.dll")]
-    private static extern uint LsaClose(IntPtr policyHandle);
+    [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+    private static extern bool CreateProcessWithTokenW(
+        IntPtr token,
+        uint logonFlags,
+        string applicationName,
+        StringBuilder commandLine,
+        uint creationFlags,
+        IntPtr environment,
+        string currentDirectory,
+        ref STARTUPINFO startupInfo,
+        out PROCESS_INFORMATION processInformation);
 
-    public static void SetBatchLogonRight(string sidText, bool add)
+    [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+    private static extern bool ConvertStringSidToSidW(
+        string stringSid,
+        out IntPtr sid);
+
+    [DllImport("kernel32.dll")]
+    private static extern IntPtr LocalFree(IntPtr memory);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern uint WaitForSingleObject(IntPtr handle, uint milliseconds);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern bool GetExitCodeProcess(IntPtr process, out uint exitCode);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern bool CloseHandle(IntPtr handle);
+
+    public static int Run(string application, string arguments, string currentDirectory)
     {
-        const uint POLICY_CREATE_ACCOUNT = 0x00000010;
-        const uint POLICY_LOOKUP_NAMES = 0x00000800;
-        var attributes = new LSA_OBJECT_ATTRIBUTES();
-        attributes.Length = Marshal.SizeOf(attributes);
-        IntPtr policy;
-        uint status = LsaOpenPolicy(
-            IntPtr.Zero,
-            ref attributes,
-            POLICY_CREATE_ACCOUNT | POLICY_LOOKUP_NAMES,
-            out policy);
-        if (status != 0)
-            throw new Win32Exception((int)LsaNtStatusToWinError(status));
+        const uint TOKEN_ASSIGN_PRIMARY = 0x0001;
+        const uint TOKEN_DUPLICATE = 0x0002;
+        const uint TOKEN_QUERY = 0x0008;
+        const uint TOKEN_ADJUST_DEFAULT = 0x0080;
+        const uint TOKEN_ADJUST_SESSIONID = 0x0100;
+        const uint DISABLE_MAX_PRIVILEGE = 0x00000001;
+        const uint CREATE_NO_WINDOW = 0x08000000;
+        const uint WAIT_FAILED = 0xffffffff;
+        const uint ERROR_PRIVILEGE_NOT_HELD = 1314;
 
-        IntPtr sid = IntPtr.Zero;
-        IntPtr rightBuffer = IntPtr.Zero;
+        IntPtr sourceToken = IntPtr.Zero;
+        IntPtr restrictedToken = IntPtr.Zero;
+        IntPtr administratorsSid = IntPtr.Zero;
+        PROCESS_INFORMATION processInformation = new PROCESS_INFORMATION();
         try
         {
-            var securityIdentifier = new SecurityIdentifier(sidText);
-            var sidBytes = new byte[securityIdentifier.BinaryLength];
-            securityIdentifier.GetBinaryForm(sidBytes, 0);
-            sid = Marshal.AllocHGlobal(sidBytes.Length);
-            Marshal.Copy(sidBytes, 0, sid, sidBytes.Length);
+            uint access = TOKEN_ASSIGN_PRIMARY | TOKEN_DUPLICATE | TOKEN_QUERY |
+                TOKEN_ADJUST_DEFAULT | TOKEN_ADJUST_SESSIONID;
+            if (!OpenProcessToken(GetCurrentProcess(), access, out sourceToken))
+                throw new Win32Exception(Marshal.GetLastWin32Error(),
+                    "failed to open the current process token");
+            if (!ConvertStringSidToSidW("S-1-5-32-544", out administratorsSid))
+                throw new Win32Exception(Marshal.GetLastWin32Error(),
+                    "failed to resolve the built-in Administrators SID");
 
-            const string rightName = "SeBatchLogonRight";
-            rightBuffer = Marshal.StringToHGlobalUni(rightName);
-            var right = new LSA_UNICODE_STRING
-            {
-                Length = checked((ushort)(rightName.Length * sizeof(char))),
-                MaximumLength = checked((ushort)((rightName.Length + 1) * sizeof(char))),
-                Buffer = rightBuffer
+            var disabledSids = new[] {
+                new SID_AND_ATTRIBUTES { Sid = administratorsSid, Attributes = 0 }
             };
-            var rights = new[] { right };
-            status = add
-                ? LsaAddAccountRights(policy, sid, rights, 1)
-                : LsaRemoveAccountRights(policy, sid, false, rights, 1);
-            if (status != 0)
-                throw new Win32Exception((int)LsaNtStatusToWinError(status));
+            if (!CreateRestrictedToken(sourceToken, DISABLE_MAX_PRIVILEGE,
+                    1, disabledSids, 0, IntPtr.Zero, 0, IntPtr.Zero,
+                    out restrictedToken))
+                throw new Win32Exception(Marshal.GetLastWin32Error(),
+                    "failed to create a restricted Windows token");
+            if (!IsTokenRestricted(restrictedToken))
+                throw new InvalidOperationException(
+                    "Windows did not mark the child token as restricted");
+
+            var startupInfo = new STARTUPINFO();
+            startupInfo.cb = Marshal.SizeOf(startupInfo);
+            var commandLine = new StringBuilder(
+                "\"" + application + "\" " + arguments);
+            bool created = CreateProcessAsUserW(
+                restrictedToken, application, commandLine,
+                IntPtr.Zero, IntPtr.Zero, false, CREATE_NO_WINDOW,
+                IntPtr.Zero, currentDirectory, ref startupInfo,
+                out processInformation);
+            int createError = created ? 0 : Marshal.GetLastWin32Error();
+            if (!created && createError == ERROR_PRIVILEGE_NOT_HELD)
+            {
+                commandLine = new StringBuilder(
+                    "\"" + application + "\" " + arguments);
+                created = CreateProcessWithTokenW(
+                    restrictedToken, 0, application, commandLine,
+                    CREATE_NO_WINDOW, IntPtr.Zero, currentDirectory,
+                    ref startupInfo, out processInformation);
+                createError = created ? 0 : Marshal.GetLastWin32Error();
+            }
+            if (!created)
+                throw new Win32Exception(createError,
+                    "failed to launch the restricted Windows process");
+
+            uint waitResult = WaitForSingleObject(
+                processInformation.hProcess, 15 * 60 * 1000);
+            if (waitResult == WAIT_FAILED)
+                throw new Win32Exception(Marshal.GetLastWin32Error(),
+                    "failed while waiting for the restricted Windows process");
+            if (waitResult != 0)
+                throw new TimeoutException(
+                    "restricted Windows process exceeded its test deadline");
+            uint exitCode;
+            if (!GetExitCodeProcess(processInformation.hProcess, out exitCode))
+                throw new Win32Exception(Marshal.GetLastWin32Error(),
+                    "failed to read the restricted Windows process exit code");
+            return checked((int)exitCode);
         }
         finally
         {
-            if (rightBuffer != IntPtr.Zero)
-                Marshal.FreeHGlobal(rightBuffer);
-            if (sid != IntPtr.Zero)
-                Marshal.FreeHGlobal(sid);
-            LsaClose(policy);
+            if (processInformation.hThread != IntPtr.Zero)
+                CloseHandle(processInformation.hThread);
+            if (processInformation.hProcess != IntPtr.Zero)
+                CloseHandle(processInformation.hProcess);
+            if (administratorsSid != IntPtr.Zero)
+                LocalFree(administratorsSid);
+            if (restrictedToken != IntPtr.Zero)
+                CloseHandle(restrictedToken);
+            if (sourceToken != IntPtr.Zero)
+                CloseHandle(sourceToken);
         }
     }
 }
@@ -130,65 +230,34 @@ if (-not (Test-Path -LiteralPath $RuntimeBin -PathType Container)) {
     throw "native runtime directory not found"
 }
 if (Test-Path -LiteralPath $TestRoot) {
-    throw "isolated standard-user test root already exists"
+    throw "isolated restricted-token test root already exists"
 }
-
-$suffix = [System.Diagnostics.Process]::GetCurrentProcess().Id
-$userName = "qwcacltest$suffix"
-$account = "$env:COMPUTERNAME\$userName"
-$taskName = "qwc-epose-acl-$suffix"
-$runtimeRoot = Join-Path $TestRoot "runtime"
-$wrapper = Join-Path $TestRoot "run-standard-user.ps1"
-$result = Join-Path $TestRoot "result.txt"
-$finished = Join-Path $TestRoot "finished.txt"
-$registered = $false
-$createdUser = $false
-$batchLogonGranted = $false
 
 function Quote-PowerShellLiteral([string]$Value) {
     return $Value.Replace("'", "''")
 }
 
-try {
-    New-Item -ItemType Directory -Path $TestRoot | Out-Null
-    $testUser = New-LocalUser -Name $userName -NoPassword -AccountNeverExpires
-    $createdUser = $true
-    $usersGroup = Get-LocalGroup -SID 'S-1-5-32-545'
-    Add-LocalGroupMember -Group $usersGroup -Member $account
-    $administrators = @(Get-LocalGroupMember -Group (
-        Get-LocalGroup -SID 'S-1-5-32-544'))
-    if ($administrators.SID.Value -contains $testUser.SID.Value) {
-        throw "temporary test account unexpectedly belongs to Administrators"
-    }
-    [QwcLsaRights]::SetBatchLogonRight($testUser.SID.Value, $true)
-    $batchLogonGranted = $true
+New-Item -ItemType Directory -Path $TestRoot | Out-Null
+$runtimeRoot = Join-Path $TestRoot "runtime"
+$wrapper = Join-Path $TestRoot "run-restricted-user.ps1"
+$result = Join-Path $TestRoot "result.txt"
+$finished = Join-Path $TestRoot "finished.txt"
 
-    & icacls.exe $TestRoot /inheritance:r |
-        Out-Null
-    & icacls.exe $TestRoot /grant:r `
-        "${account}:(OI)(CI)F" `
-        '*S-1-5-18:(OI)(CI)F' `
-        '*S-1-5-32-544:(OI)(CI)F' |
-        Out-Null
-    if ($LASTEXITCODE -ne 0) {
-        throw "failed to isolate the standard-user test directory"
-    }
-
-    $runtimeScript = Quote-PowerShellLiteral (
-        (Resolve-Path ./tests/epose/windows_keystore_runtime.ps1).Path)
-    $daemon = Quote-PowerShellLiteral ((Resolve-Path $DaemonPath).Path)
-    $runtime = Quote-PowerShellLiteral ((Resolve-Path $RuntimeBin).Path)
-    $test = Quote-PowerShellLiteral $runtimeRoot
-    $resultPath = Quote-PowerShellLiteral $result
-    $finishedPath = Quote-PowerShellLiteral $finished
-    $wrapperBody = @"
+$runtimeScript = Quote-PowerShellLiteral (
+    (Resolve-Path ./tests/epose/windows_keystore_runtime.ps1).Path)
+$daemon = Quote-PowerShellLiteral ((Resolve-Path $DaemonPath).Path)
+$runtime = Quote-PowerShellLiteral ((Resolve-Path $RuntimeBin).Path)
+$test = Quote-PowerShellLiteral $runtimeRoot
+$resultPath = Quote-PowerShellLiteral $result
+$finishedPath = Quote-PowerShellLiteral $finished
+$wrapperBody = @"
 `$ErrorActionPreference = 'Stop'
 try {
     `$principal = [Security.Principal.WindowsPrincipal]::new(
         [Security.Principal.WindowsIdentity]::GetCurrent())
     if (`$principal.IsInRole(
             [Security.Principal.WindowsBuiltInRole]::Administrator)) {
-        throw 'scheduled keystore test unexpectedly has administrator membership'
+        throw 'restricted keystore test unexpectedly has administrator membership'
     }
     `$env:PATH = '$runtime;' + `$env:PATH
     & '$runtimeScript' -Mode VerifyFixed -DaemonPath '$daemon' -TestRoot '$test' |
@@ -197,63 +266,34 @@ try {
 } catch {
     `$safe = `$_.Exception.Message
     if (`$safe -match '(?i)operator_secret_key|service_secret_key') {
-        `$safe = 'standard-user test failed; secret-bearing diagnostic suppressed'
+        `$safe = 'restricted-token test failed; secret-bearing diagnostic suppressed'
     }
     `$safe = `$safe -replace '(?i)\b[0-9a-f]{64}\b', '<redacted-hex64>'
     `$safe | Set-Content -LiteralPath '$resultPath' -Encoding UTF8
     '1' | Set-Content -LiteralPath '$finishedPath' -Encoding ASCII
 }
 "@
-    Set-Content -LiteralPath $wrapper -Value $wrapperBody -Encoding UTF8
+Set-Content -LiteralPath $wrapper -Value $wrapperBody -Encoding UTF8
 
-    $pwsh = (Get-Command pwsh.exe -ErrorAction Stop).Source
-    $arguments = "-NoLogo -NoProfile -NonInteractive -ExecutionPolicy Bypass -File `"$wrapper`""
-    $action = New-ScheduledTaskAction -Execute $pwsh -Argument $arguments
-    $principal = New-ScheduledTaskPrincipal `
-        -UserId $account -LogonType S4U -RunLevel Limited
-    $settings = New-ScheduledTaskSettingsSet `
-        -ExecutionTimeLimit (New-TimeSpan -Minutes 15) `
-        -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
-    Register-ScheduledTask -TaskName $taskName -Action $action `
-        -Principal $principal -Settings $settings | Out-Null
-    $registered = $true
-    Start-ScheduledTask -TaskName $taskName
-
-    $deadline = [DateTime]::UtcNow.AddMinutes(15)
-    while ([DateTime]::UtcNow -lt $deadline -and
-            -not (Test-Path -LiteralPath $finished -PathType Leaf)) {
-        Start-Sleep -Milliseconds 500
-    }
-    if (-not (Test-Path -LiteralPath $finished -PathType Leaf)) {
-        $info = Get-ScheduledTaskInfo -TaskName $taskName
-        throw "standard-user task timed out (last result $($info.LastTaskResult))"
-    }
-    $exitCode = (Get-Content -LiteralPath $finished -Raw).Trim()
-    $output = if (Test-Path -LiteralPath $result -PathType Leaf) {
-        (Get-Content -LiteralPath $result -Raw).Trim()
-    } else {
-        "no safe standard-user diagnostic was available"
-    }
-    if ($output -match '(?i)operator_secret_key|service_secret_key') {
-        throw "secret key material appeared in the standard-user result"
-    }
-    if ($exitCode -ne "0") {
-        throw "standard-user runtime failed: $output"
-    }
-    if ($output -notmatch 'fixed_runtime=pass starts=4 identities=stable content_sha256=stable broad_acl=rejected acl_only_repair=pass') {
-        throw "standard-user runtime did not report the complete proof"
-    }
-    Write-Output "standard_user_runtime=pass account=non_administrator starts=4 identities=stable"
-} finally {
-    if ($registered) {
-        Stop-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue
-        Unregister-ScheduledTask -TaskName $taskName -Confirm:$false `
-            -ErrorAction SilentlyContinue
-    }
-    if ($batchLogonGranted) {
-        [QwcLsaRights]::SetBatchLogonRight($testUser.SID.Value, $false)
-    }
-    if ($createdUser) {
-        Remove-LocalUser -Name $userName -ErrorAction SilentlyContinue
-    }
+$pwsh = (Get-Command pwsh.exe -ErrorAction Stop).Source
+$arguments = "-NoLogo -NoProfile -NonInteractive -ExecutionPolicy Bypass -File `"$wrapper`""
+$processExit = [QwcRestrictedProcess]::Run($pwsh, $arguments, $TestRoot)
+if (-not (Test-Path -LiteralPath $finished -PathType Leaf)) {
+    throw "restricted-token runtime produced no completion marker"
 }
+$exitCode = (Get-Content -LiteralPath $finished -Raw).Trim()
+$output = if (Test-Path -LiteralPath $result -PathType Leaf) {
+    (Get-Content -LiteralPath $result -Raw).Trim()
+} else {
+    "no safe restricted-token diagnostic was available"
+}
+if ($output -match '(?i)operator_secret_key|service_secret_key') {
+    throw "secret key material appeared in the restricted-token result"
+}
+if ($processExit -ne 0 -or $exitCode -ne "0") {
+    throw "restricted-token runtime failed: $output"
+}
+if ($output -notmatch 'fixed_runtime=pass starts=4 identities=stable content_sha256=stable broad_acl=rejected acl_only_repair=pass') {
+    throw "restricted-token runtime did not report the complete proof"
+}
+Write-Output "restricted_windows_token=pass administrator_membership=disabled starts=4 identities=stable"
